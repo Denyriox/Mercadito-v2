850ac9f9dbb0360c600f6bf00f788ceb49a04083
Denyriox <kamiltraviesoac@gmail.com>
2025-08-22 00:37:09 -0400

    PWA

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('mercadito-v2-cache').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/style.css',
+        '/script.js',
+        '/productos.json',
+        '/manifest.webmanifest',
+        '/icon-192.png',
+        '/icon-512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
